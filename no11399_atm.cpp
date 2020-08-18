64a5eaf3fab6bccdb9359dee5f94987d54164161
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-08-18 19:20:23 +0900

    Solve problem No.11399 'ATM'

---
+++ no11399_atm.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int numPerson;
+    int totalTime = 0;
+
+    cin >> numPerson;
+
+    vector<int> withdrawalTimes(numPerson);
+
+    for (int &withdrawalTime : withdrawalTimes) {
+        cin >> withdrawalTime;
+    }
+
+    sort(withdrawalTimes.begin(), withdrawalTimes.end());
+
+    for (int i = 0; i < numPerson; i++) {
+        totalTime += (numPerson - i) * withdrawalTimes[i];
+    }
+
+    cout << totalTime << endl;
+
+    return 0;
+}
