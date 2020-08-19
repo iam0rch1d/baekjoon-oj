f75ae33a0a88c6977b8f62026dd9cd4cc32c4698
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-08-19 09:55:45 +0900

    Solve problem No.1541 'Missing Brackets'

---
+++ no1541_missing_brackets.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#define MAXIMUM_NUM_OPERAND 25
+
+using namespace std;
+
+int main() {
+    string formula;
+    int operand = 0;
+    vector<int> operands;
+    int indexFirstMinus = MAXIMUM_NUM_OPERAND;
+    int operationResult = 0;
+
+    cin >> formula;
+
+    for (int i = 0; i < formula.size(); i++) {
+        if (formula.at(i) >= '0' && formula.at(i) <= '9') {
+            operand *= 10;
+            operand += formula.at(i) - '0';
+        }
+
+        if (formula.at(i) < '0' || formula.at(i) > '9' || i == formula.size() - 1) {
+            operands.push_back(operand);
+            operand = 0;
+        }
+
+        if (formula.at(i) == '-' && indexFirstMinus == MAXIMUM_NUM_OPERAND) indexFirstMinus = operands.size();
+    }
+
+    for (int i = 0; i < operands.size(); i++) {
+        if (i < indexFirstMinus) operationResult += operands[i];
+        else operationResult -= operands[i];
+    }
+
+    cout << operationResult << endl;
+
+    return 0;
+}
