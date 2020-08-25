2f0c0fb622949c399a612bba265b2ece5c15f018
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-08-25 20:48:09 +0900

    Solve problem No.5086 'Factor or Multiple'

---
+++ no5086_factor_or_multiple.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    while (true) {
+        int numbers[2];
+
+        cin >> numbers[0] >> numbers[1];
+
+        if (numbers[0] == 0 && numbers[1] == 0) return 0;
+
+        cout << ((numbers[0] < numbers[1] && numbers[1] % numbers[0] == 0) ? "factor" :
+        (numbers[0] > numbers[1] && numbers[0] % numbers[1] == 0) ? "multiple" : "neither") << endl;
+    }
+}
