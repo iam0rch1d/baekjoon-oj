a2b9720f17b6d30cb8d81e00f0336759ce9cbf1f
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-08-28 02:40:53 +0900

    Solve problem No.10828 'Stack'

---
+++ no10828_stack.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int numInstruction;
+    vector<int> stack;
+
+    cin >> numInstruction;
+
+    for (int instructionNo = 0; instructionNo < numInstruction; instructionNo++) {
+        string instruction;
+
+        cin >> instruction;
+
+        if (instruction == "push") {
+            int target;
+
+            cin >> target;
+
+            stack.push_back(target);
+        } else if (instruction == "pop"){
+            cout << (stack.empty() ? -1 : stack[stack.size() - 1]) << endl;
+
+            if (!stack.empty()) stack.pop_back();
+        } else if (instruction == "size") cout << stack.size() << endl;
+        else if (instruction == "empty") cout << (stack.empty() ? 1 : 0) << endl;
+        else if (instruction == "top") cout << (stack.empty() ? -1 : stack[stack.size() - 1]) << endl;
+    }
+
+    return 0;
+}
