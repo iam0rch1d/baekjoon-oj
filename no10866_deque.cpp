061e10540024ca577ef893c91b45c653dded29f7
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-08-31 02:52:53 +0900

    Solve problem No.10866 'Deque'

---
+++ no10866_deque.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int numInstruction;
+    deque<int> deque;
+
+    cin >> numInstruction;
+
+    while (numInstruction-- > 0) {
+        string instruction;
+
+        cin >> instruction;
+
+        if (instruction == "push_front") {
+            int target;
+
+            cin >> target;
+
+            deque.push_front(target);
+        } else if (instruction == "push_back") {
+            int target;
+
+            cin >> target;
+
+            deque.push_back(target);
+        } else if (instruction == "pop_front"){
+            cout << (deque.empty() ? -1 : deque.front()) << "\n";
+
+            if (!deque.empty()) deque.pop_front();
+        } else if (instruction == "pop_back"){
+            cout << (deque.empty() ? -1 : deque.back()) << "\n";
+
+            if (!deque.empty()) deque.pop_back();
+        } else if (instruction == "size") cout << deque.size() << "\n";
+        else if (instruction == "empty") cout << (deque.empty() ? 1 : 0) << "\n";
+        else if (instruction == "front") cout << (deque.empty() ? -1 : deque.front()) << "\n";
+        else if (instruction == "back") cout << (deque.empty() ? -1 : deque.back()) << "\n";
+    }
+
+    return 0;
+}
