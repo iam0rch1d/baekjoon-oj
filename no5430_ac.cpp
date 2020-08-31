074f106d50a08f7a8990d6d5bda95d3e93293664
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-01 00:03:56 +0900

    Solve problem No.5430 'AC'

---
+++ no5430_ac.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    int numTestcase;
+
+    cin >> numTestcase;
+
+    while (numTestcase-- > 0) {
+        string functions;
+        int arraySize;
+        string arrayString;
+        string::iterator function;
+        int element = 0;
+        deque<int> array;
+        bool shouldPopBack = false;
+        bool hasError = false;
+
+        cin >> functions >> arraySize >> arrayString;
+
+        function = functions.begin();
+
+        for (int i = 1; i < arrayString.size() - 1; i++) {
+            if (arrayString.at(i) >= '0' && arrayString.at(i) <= '9') {
+                element *= 10;
+                element += arrayString.at(i) - '0';
+            }
+
+            if (arrayString.at(i) < '0' || arrayString.at(i) > '9' || i == arrayString.size() - 2) {
+                array.push_back(element);
+
+                element = 0;
+            }
+        }
+
+        while (*function != '\0') {
+            if (*function == 'R') shouldPopBack = !shouldPopBack;
+            else if (array.empty()) {
+                hasError = true;
+
+                break;
+            } else if (shouldPopBack) array.pop_back();
+            else array.pop_front();
+
+            function++;
+        }
+
+        if (hasError) {
+            cout << "error" << endl;
+
+            continue;
+        }
+
+        cout << "[";
+
+        if (shouldPopBack) {
+            while (!array.empty()) {
+                cout << array.back();
+
+                array.pop_back();
+
+                if (!array.empty()) cout << ",";
+            }
+        } else {
+            while (!array.empty()) {
+                cout << array.front();
+
+                array.pop_front();
+
+                if (!array.empty()) cout << ",";
+            }
+        }
+
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
