7235b5d7872382c616036b4e44b928c25b868d2e
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-06 11:15:49 +0900

    Solve problem No.9095 'Add One-two-three'

---
+++ no9095_add_one_two_three.cpp
@@ -0,0 +1,40 @@
+#include <cstring>
+#include <iostream>
+
+#define UNMEMOIZED -1
+
+using namespace std;
+
+int methodCountCache[11];
+
+int memoizeMethodCount(int target) {
+    if (target < 3) return target;
+
+    if (target == 3) return 4;
+
+    int &methodCount = methodCountCache[target];
+
+    if (methodCount != UNMEMOIZED) return methodCount;
+
+    return methodCount = memoizeMethodCount(target - 1)
+                         + memoizeMethodCount(target - 2)
+                         + memoizeMethodCount(target - 3);
+}
+
+int main() {
+    int numTestcase;
+
+    cin >> numTestcase;
+
+    memset(methodCountCache, UNMEMOIZED, sizeof(methodCountCache));
+
+    while (numTestcase-- > 0) {
+        int target;
+
+        cin >> target;
+
+        cout << memoizeMethodCount(target) << endl;
+    }
+
+    return 0;
+}
