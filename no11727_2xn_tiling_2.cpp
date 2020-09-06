eaf3e3cd7c333093f1772b865cd27ac6e40b1120
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-06 12:40:52 +0900

    Solve problem No.11727 '2XN Tiling 2'

---
+++ no11727_2xn_tiling_2.cpp
@@ -0,0 +1,33 @@
+#include <cstring>
+#include <iostream>
+
+#define UNMEMOIZED -1
+#define MODULUS 10007
+
+using namespace std;
+
+int tilingCountCache[1001];
+
+int memoizeTilingCount(int width) {
+    if (width == 1) return 1;
+
+    if (width == 2) return 3;
+
+    int &tilingCount = tilingCountCache[width];
+
+    if (tilingCount != UNMEMOIZED) return tilingCount;
+
+    return tilingCount = (memoizeTilingCount(width - 1) + 2 * memoizeTilingCount(width - 2)) % MODULUS;
+}
+
+int main() {
+    int width;
+
+    memset(tilingCountCache, UNMEMOIZED, sizeof(tilingCountCache));
+
+    cin >> width;
+
+    cout << memoizeTilingCount(width) << endl;
+
+    return 0;
+}
