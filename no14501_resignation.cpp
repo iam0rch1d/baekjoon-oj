72230cb4717360a17977f159039883bcb4399a7d
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-06 15:19:40 +0900

    Solve problem No.14501 'Resignation', Revise comment

---
+++ no14501_resignation.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+
+#define UNMEMOIZED -1
+
+using namespace std;
+
+int maximumTotalPayCache[16];
+
+int main() {
+    int numDay;
+    pair<int, int> consultations[15];
+
+    cin >> numDay;
+
+    for (int i = 0; i < numDay; i++) {
+        cin >> consultations[i].first >> consultations[i].second; // .first = <requiredDays>, .second = <pay>
+
+        maximumTotalPayCache[i + 1] = max(maximumTotalPayCache[i + 1], maximumTotalPayCache[i]);
+        maximumTotalPayCache[i + consultations[i].first] = max(maximumTotalPayCache[i + consultations[i].first],
+                                                               maximumTotalPayCache[i] + consultations[i].second);
+    }
+
+    cout << *max_element(maximumTotalPayCache, maximumTotalPayCache + numDay + 1) << endl;
+
+    return 0;
+}
