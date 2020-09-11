bb23cd21729e263d71372bf9affb55b6e3177a25
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-12 07:18:42 +0900

    Solve problem No.1654 'Cable Cropping'

---
+++ no1654_cable_cropping.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int numOwnedCable;
+long long ownedCables[10000];
+long long numNeededCable;
+
+int main() {
+    long long croppingSizeLow = 1;
+    long long croppingSizeHigh;
+    long long maximumCroppingSize = 0;
+
+    cin >> numOwnedCable >> numNeededCable;
+
+    for (int i = 0; i < numOwnedCable; i++) {
+        cin >> ownedCables[i];
+    }
+
+    croppingSizeHigh = *max_element(ownedCables, ownedCables + numOwnedCable);
+
+    while (croppingSizeLow <= croppingSizeHigh) {
+        long long croppedCableCount = 0;
+        long long croppingSizeCenter = (croppingSizeLow + croppingSizeHigh) / 2;
+
+        for (int i = 0; i < numOwnedCable; i++) {
+            croppedCableCount += ownedCables[i] / croppingSizeCenter;
+        }
+
+        if (croppedCableCount >= numNeededCable) {
+            maximumCroppingSize = max(maximumCroppingSize, croppingSizeCenter);
+            croppingSizeLow = croppingSizeCenter + 1;
+        } else croppingSizeHigh = croppingSizeCenter - 1;
+    }
+
+    cout << maximumCroppingSize << endl;
+
+    return 0;
+}
