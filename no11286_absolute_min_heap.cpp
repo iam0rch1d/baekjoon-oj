28f0b68ae3284dad1999e4d6827539f1aaeb0952
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-15 18:17:38 +0900

    Solve problem No.1927 'Min-Heap', No.11279 'Max-Heap', No.11286 'Absolute Min-Heap'

---
+++ no11286_absolute_min_heap.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+typedef struct Node {
+    int value;
+} Node;
+
+bool operator<(Node a, Node b) {
+    return abs(a.value) > abs(b.value) || (abs(a.value) == abs(b.value) && a.value > b.value);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int numInstruction;
+    priority_queue<Node> maxHeap;
+
+    cin >> numInstruction;
+
+    while (numInstruction-- > 0) {
+        int instruction;
+
+        cin >> instruction;
+
+        if (instruction == 0 && !maxHeap.empty()) {
+            cout << maxHeap.top().value << "\n";
+
+            maxHeap.pop();
+        } else if (instruction == 0) cout << 0 << "\n";
+        else maxHeap.push({instruction});
+    }
+
+    return 0;
+}
---
+++ no1927_min_heap.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int numInstruction;
+    priority_queue<int, vector<int>, greater<>> maxHeap;
+
+    cin >> numInstruction;
+
+    while (numInstruction-- > 0) {
+        int instruction;
+
+        cin >> instruction;
+
+        if (instruction == 0 && !maxHeap.empty()) {
+            cout << maxHeap.top() << "\n";
+
+            maxHeap.pop();
+        } else if (instruction == 0) cout << 0 << "\n";
+        else maxHeap.push(instruction);
+    }
+
+    return 0;
+}
