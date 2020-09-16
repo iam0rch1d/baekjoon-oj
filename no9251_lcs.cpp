a331a33b95689b5c6a7712d4c3d3762305340ce5
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-16 10:14:45 +0900

    Refactor code

--- no9251_lcs.cpp
+++ no9251_lcs.cpp
@@ -10,10 +10,10 @@ int main() {
 
     cin >> a >> b;
 
-    for (int i = 0; i < a.size(); i++) {
-        for (int j = 0; j < b.size(); j++) {
-            if (a[i] == b[j]) lcsLengthCache[i + 1][j + 1] = lcsLengthCache[i][j] + 1;
-            else lcsLengthCache[i + 1][j + 1] = max(lcsLengthCache[i][j + 1], lcsLengthCache[i + 1][j]);
+    for (int i = 1; i <= a.size(); i++) {
+        for (int j = 1; j <= b.size(); j++) {
+            if (a[i - 1] == b[j - 1]) lcsLengthCache[i][j] = lcsLengthCache[i - 1][j - 1] + 1;
+            else lcsLengthCache[i][j] = max(lcsLengthCache[i - 1][j], lcsLengthCache[i][j - 1]);
         }
     }
 
