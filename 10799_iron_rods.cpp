d80bfd1afebb8a5818ff1a0ba1478c5a780e4eef
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-20 22:19:37 +0900

    Solve problem No.10799 'Iron Rods'

---
+++ 10799_iron_rods.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string brackets;
+    int rodStack = -1;
+    int totalPieceCount = 0;
+
+    cin >> brackets;
+
+    for (int i = 0; i < brackets.size(); i++) {
+        if (brackets[i] == '(') rodStack++;
+        else {
+            if (brackets[i - 1] == '(') {
+                totalPieceCount += rodStack;
+            } else totalPieceCount++;
+
+            rodStack--;
+        }
+    }
+
+    cout << totalPieceCount << endl;
+
+    return 0;
+}
