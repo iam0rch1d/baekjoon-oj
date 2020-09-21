49d145210031ca44ba6c2c9355343b1a79c165c1
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-21 20:47:35 +0900

    Solve problem No.15988 'Add One, Two, Three 3'

---
+++ 15988_add_one_two_three_3.cpp
@@ -0,0 +1,40 @@
+#include <cstring>
+#include <iostream>
+
+#define UNMEMOIZED -1
+#define MODULO 1000000009
+
+using namespace std;
+
+long long methodCountCache[1000001];
+
+long long memoizeMethodCount(int target) {
+    if (target <= 2) return target;
+    else if (target == 3) return 4;
+
+    long long &methodCount = methodCountCache[target];
+
+    if (methodCount != UNMEMOIZED) return methodCount;
+
+    return methodCount = (memoizeMethodCount(target - 1)
+                          + memoizeMethodCount(target - 2)
+                          + memoizeMethodCount(target - 3)) % MODULO;
+}
+
+int main() {
+    int numTestcase;
+
+    cin >> numTestcase;
+
+    memset(methodCountCache, UNMEMOIZED, sizeof(methodCountCache));
+
+    while (numTestcase-- > 0) {
+        int target;
+
+        cin >> target;
+
+        cout << memoizeMethodCount(target) << endl;
+    }
+
+    return 0;
+}
