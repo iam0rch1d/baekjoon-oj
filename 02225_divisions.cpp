19dab959312de989492fe979eae6437b211eaf4a
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-21 19:56:19 +0900

    Solve problem No.2225 'Divisions'

---
+++ 02225_divisions.cpp
@@ -0,0 +1,33 @@
+#include <cstring>
+#include <iostream>
+
+#define UNMEMOIZED -1
+#define MODULO 1000000000
+
+using namespace std;
+
+long long binomialCoefficientCache[401][401];
+
+long long memoizeBinomialCoefficient(int n, int k) {
+    if (k == 0 || n == k) return 1;
+
+    long long &binomialCoefficient = binomialCoefficientCache[n][k];
+
+    if (binomialCoefficient != UNMEMOIZED) return binomialCoefficient;
+
+    return binomialCoefficient = (memoizeBinomialCoefficient(n - 1, k - 1)
+                                  + memoizeBinomialCoefficient(n - 1, k)) % MODULO;
+}
+
+int main() {
+    int n;
+    int k;
+
+    memset(binomialCoefficientCache, UNMEMOIZED, sizeof(binomialCoefficientCache));
+
+    cin >> n >> k;
+
+    cout << memoizeBinomialCoefficient(n + k - 1, n) << endl;
+
+    return 0;
+}
