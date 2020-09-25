89f0c03d614708d286ce66b22ae03654c9765282
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-25 09:45:05 +0900

    Solve problem No.10820 'String Analysis'

---
+++ 10820_string_analysis.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string buffer;
+
+    while (getline(cin, buffer)) {
+        string::iterator head = buffer.begin();
+        int lowercaseCount = 0;
+        int uppercaseCount = 0;
+        int digitCount = 0;
+        int spaceCount = 0;
+
+        while (*head != '\0'){
+            if (*head >= 'a' && *head <= 'z') lowercaseCount++;
+            else if (*head >= 'A' && *head <= 'Z') uppercaseCount++;
+            else if (*head >= '0' && *head <= '9') digitCount++;
+            else spaceCount++;
+
+            head++;
+        }
+
+        cout << lowercaseCount << ' ' << uppercaseCount << ' ' << digitCount << ' ' << spaceCount << '\n';
+    }
+
+    return 0;
+}
