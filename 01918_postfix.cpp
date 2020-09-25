973e2e3049cfb56d1d2317f7b9d3048698ab0006
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-09-25 09:27:48 +0900

    Solve problem No.1918 'Postfix'

---
+++ 01918_postfix.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int calculatePriority(char symbol) {
+    if (symbol == '(') return 0;
+    else if (symbol == '+' || symbol == '-') return 1;
+    else return 2;
+}
+
+int main() {
+    string infix;
+    string::iterator head;
+    stack<char> shuntingYard;
+    string postfix;
+
+    cin >> infix;
+
+    head = infix.begin();
+
+    while (*head != '\0') {
+        if (*head >= 'A' && *head <= 'Z') postfix += *head;
+        else if (*head == '(') shuntingYard.push(*head);
+        else if (*head == ')') {
+            while (!shuntingYard.empty()) {
+                char symbol = shuntingYard.top();
+
+                shuntingYard.pop();
+
+                if (symbol == '(') break;
+
+                postfix += symbol;
+            }
+        } else {
+            while (!shuntingYard.empty() && calculatePriority(shuntingYard.top()) >= calculatePriority(*head)) {
+                postfix += shuntingYard.top();
+
+                shuntingYard.pop();
+            }
+
+            shuntingYard.push(*head);
+        }
+
+        head++;
+    }
+
+    while (!shuntingYard.empty()) {
+        postfix += shuntingYard.top();
+
+        shuntingYard.pop();
+    }
+
+    cout << postfix << '\n';
+
+    return 0;
+}
