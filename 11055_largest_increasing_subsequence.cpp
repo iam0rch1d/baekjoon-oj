01505eb166dd5e92858aa847b538a02eb21b232c
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-10-02 11:24:45 +0900

    Solve problem No.11055 'Largest Increasing Subsequence'

---
+++ 11055_largest_increasing_subsequence.cpp
@@ -0,0 +1,42 @@
+#include <cstring>
+#include <iostream>
+
+#define UNMEMOIZED -1
+
+using namespace std;
+
+int sequenceSize;
+int sequence[1001];
+int maximumSumCache[1001];
+
+int memoizeMaximumSum(int toIndex) {
+    if (toIndex == 0) return sequence[0];
+
+    int &maximumSum = maximumSumCache[toIndex];
+
+    if (maximumSum != UNMEMOIZED) return maximumSum;
+
+    maximumSum = 0;
+
+    for (int i = toIndex - 1; i >= 0; i--) {
+        if (sequence[i] < sequence[toIndex]) maximumSum = max(maximumSum, memoizeMaximumSum(i));
+    }
+
+    return maximumSum += sequence[toIndex];
+}
+
+int main() {
+    cin >> sequenceSize;
+
+    memset(maximumSumCache, UNMEMOIZED, sizeof(maximumSumCache));
+
+    for (int i = 0; i < sequenceSize; i++) {
+        cin >> sequence[i];
+    }
+
+    sequence[sequenceSize] = 1e6;
+
+    cout << memoizeMaximumSum(sequenceSize) - 1e6 << '\n';
+
+    return 0;
+}
