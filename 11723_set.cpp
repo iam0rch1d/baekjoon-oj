494bb161176bc08a848140936f4b4c2a7035da56
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-10-09 08:45:16 +0900

    Solve problem No.11723 'Set'

---
+++ 11723_set.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int numInstruction;
+    unsigned bitset = 0u;
+
+    cin >> numInstruction;
+
+    while (numInstruction-- > 0) {
+        string instruction;
+        unsigned target;
+
+        cin >> instruction;
+
+        if (instruction == "add") {
+            cin >> target;
+
+            bitset |= (1u << --target);
+        } else if (instruction == "remove") {
+            cin >> target;
+
+            bitset &= ~(1u << --target);
+        } else if (instruction == "check") {
+            cin >> target;
+            cout << "01"[(bitset & (1u << --target)) != 0] << '\n';
+        } else if (instruction == "toggle") {
+            cin >> target;
+
+            bitset ^= (1u << --target);
+        } else if (instruction == "all") bitset = (1u << 20u) - 1;
+        else if (instruction == "empty") bitset = 0u;
+    }
+
+    return 0;
+}
