7503f1955e420f9a043a8662c5dc86fee03c94bf
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-10-10 02:20:25 +0900

    Solve problem No.13023 'ABCDE'

---
+++ 13023_abcde.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> adjacentVerticesOf[2000];
+int isVisited[2000];
+
+void dfsRelation(int vertex, int depth) {
+    isVisited[vertex] = true;
+
+    if (depth == 4) {
+        cout << "1\n";
+
+        exit(0);
+    }
+
+    for (int adjacentVertex : adjacentVerticesOf[vertex]) {
+        if (!isVisited[adjacentVertex]) dfsRelation(adjacentVertex, depth + 1);
+    }
+
+    isVisited[vertex] = false;
+}
+
+int main() {
+    int n;
+    int m;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int fromVertex;
+        int toVertex;
+
+        cin >> fromVertex >> toVertex;
+
+        adjacentVerticesOf[fromVertex].push_back(toVertex);
+        adjacentVerticesOf[toVertex].push_back(fromVertex);
+    }
+
+    for (int i = 0; i < n; i++) {
+        dfsRelation(i, 0);
+    }
+
+    cout << "0\n";
+
+    return 0;
+}
