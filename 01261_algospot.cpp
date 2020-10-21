6b05d0e579b70d61c47f7ba019cdabd096f71d52
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-10-21 17:55:10 +0900

    Solve problem No.1261 'Algospot'

---
+++ 01261_algospot.cpp
@@ -0,0 +1,73 @@
+#include <cstring>
+#include <deque>
+#include <iostream>
+
+#define MAX_SIZE 100
+
+using namespace std;
+
+typedef struct Point {
+    int y;
+    int x;
+} Point;
+
+Point operator+(Point a, Point b) {
+    return {a.y + b.y, a.x + b.x};
+}
+
+int breakCount[MAX_SIZE][MAX_SIZE];
+
+int main() {
+    int n;
+    int m;
+    int map[MAX_SIZE][MAX_SIZE];
+    deque<Point> bfsPoints;
+    const Point differences[] = {{0,  -1},
+                                 {1,  0},
+                                 {-1, 0},
+                                 {0,  1}};
+
+    cin >> m >> n;
+
+    for (int i = 0; i < n; i++) {
+        string mapRow;
+
+        cin >> mapRow;
+
+        for (int j = 0; j < m; j++) {
+            map[i][j] = mapRow[j] - '0';
+        }
+    }
+
+    breakCount[0][0] = 1;
+
+    bfsPoints.push_front({0, 0});
+
+    while (!bfsPoints.empty()) {
+        Point current = bfsPoints.front();
+
+        if (current.y == n - 1 && current.x == m - 1) {
+            cout << breakCount[current.y][current.x] - 1 << '\n';
+
+            return 0;
+        }
+
+        bfsPoints.pop_front();
+
+        for (Point difference : differences) {
+            Point adjacent = current + difference;
+
+            if (adjacent.y < 0
+                || adjacent.y >= n
+                || adjacent.x < 0
+                || adjacent.x >= m
+                || breakCount[adjacent.y][adjacent.x]) {
+                continue;
+            }
+
+            breakCount[adjacent.y][adjacent.x] = breakCount[current.y][current.x] + map[adjacent.y][adjacent.x];
+
+            map[adjacent.y][adjacent.x] ? bfsPoints.push_back(adjacent) : bfsPoints.push_front(adjacent);
+        }
+    }
+}
