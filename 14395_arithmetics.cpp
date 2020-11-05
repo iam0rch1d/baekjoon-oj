8cedda88b4a6de662f6934edff9ce158b33b4589
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-11-05 14:25:50 +0900

    Solve problem No.14395 'Arithmetics'

---
+++ 14395_arithmetics.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <queue>
+#include <set>
+#include <tuple>
+
+using namespace std;
+
+int main() {
+    long long s;
+    long long t;
+
+    cin >> s >> t;
+
+    if (s == t) {
+        cout << "0\n";
+
+        return 0;
+    }
+
+    set<long long> creatableNumbers;
+    queue<pair<long long, string>> bfsOperations;
+
+    creatableNumbers.insert(s);
+    bfsOperations.push({s, ""});
+
+    while (!bfsOperations.empty()) {
+        long long currentNumber;
+        string currentOperations;
+
+        tie(currentNumber, currentOperations) = bfsOperations.front();
+
+        bfsOperations.pop();
+
+        if (currentNumber == t) {
+            cout << currentOperations << '\n';
+
+            return 0;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            long long nextNumber = 0;
+
+            if (i == 0) nextNumber = currentNumber * currentNumber;
+            else if (i == 1) nextNumber = currentNumber + currentNumber;
+            else if (i == 2) nextNumber = 0;
+            else if (currentNumber) nextNumber = 1;
+
+            if (nextNumber >= 0 && nextNumber <= 1000000000 && !creatableNumbers.count(nextNumber)) {
+                creatableNumbers.insert(nextNumber);
+                bfsOperations.push({nextNumber, currentOperations + "*+-/"[i]});
+            }
+        }
+    }
+
+    cout << "-1\n";
+
+    return 0;
+}
