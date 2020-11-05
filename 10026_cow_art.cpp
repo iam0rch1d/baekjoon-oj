d646bd2d8775356ec9644ab3956bb2d818777ec8
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-11-05 09:54:41 +0900

    Solve problem No.10026 'Cow Art'

---
+++ 10026_cow_art.cpp
@@ -0,0 +1,78 @@
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+int n;
+int areaNoAt[100][100];
+
+void bfsArea(vector<string> &map, int sy, int sx, int areaNo) {
+    queue<pair<int, int>> bfsPoints;
+
+    areaNoAt[sy][sx] = areaNo;
+
+    bfsPoints.push({sy, sx});
+
+    while (!bfsPoints.empty()) {
+        int y;
+        int x;
+
+        tie(y, x) = bfsPoints.front();
+
+        bfsPoints.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + "1201"[i] - '1';
+            int nx = x + "0112"[i] - '1';
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= n || areaNoAt[ny][nx] || map[ny][nx] != map[y][x]) continue;
+
+            areaNoAt[ny][nx] = areaNo;
+
+            bfsPoints.push({ny, nx});
+        }
+    }
+}
+
+int main() {
+    int areaNo = 0;
+
+    cin >> n;
+
+    vector<string> map(n);
+
+    for (auto &mapRow : map) {
+        cin >> mapRow;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!areaNoAt[i][j]) bfsArea(map, i, j, ++areaNo);
+        }
+    }
+
+    cout << areaNo << ' ';
+
+    memset(areaNoAt, 0, sizeof(areaNoAt));
+
+    areaNo = 0;
+
+    for (auto &mapRow : map) {
+        for (int i = 0; i < n; i++) {
+            if (mapRow[i] == 'G') mapRow[i] = 'R';
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!areaNoAt[i][j]) bfsArea(map, i, j, ++areaNo);
+        }
+    }
+
+    cout << areaNo << '\n';
+
+    return 0;
+}
