40af5e17a33290b6ba3d776cee483eef71257661
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-11-07 09:55:49 +0900

    Solve problem No.2109 'Supermarket'

---
+++ 02109_supermarket.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    priority_queue<int> profits;
+    int productIndex = 0;
+    int totalProfit = 0;
+
+    cin >> n;
+
+    vector<pair<int, int>> products(n);
+
+    for (auto &product : products) {
+        cin >> product.first >> product.second;  // .first = <profit>, .second = <deadline>
+    }
+
+    sort(products.begin(), products.end(), [](auto i, auto j) { return i.second > j.second; });
+
+    for (int i = 10000; i >= 1; i--) {
+        while (productIndex < n && products[productIndex].second == i) {
+            profits.push(products[productIndex++].first);
+        }
+
+        if (!profits.empty()) {
+            totalProfit += profits.top();
+
+            profits.pop();
+        }
+    }
+
+    cout << totalProfit << '\n';
+
+    return 0;
+}
