cc5ae904cf267e5b4881ec9abe8b86cf75310b0f
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-11-09 10:47:16 +0900

    Solve problem No.1201 'NMK'

---
+++ 01201_nmk.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int n;
+    int m;
+    int k;
+    int quotient;
+    int remainder;
+
+    cin >> n >> m >> k;
+
+    if (n < m + k - 1 || n > m * k) {
+        cout << "-1\n";
+
+        return 0;
+    }
+
+    for (int i = k; i >= 1; i--) {
+        cout << i << ' ';
+    }
+
+    if (m - 1 == 0) {
+        cout << '\n';
+
+        return 0;
+    }
+
+    quotient = (n - k) / (m - 1);
+    remainder = (n - k) % (m - 1);
+
+    stack<int> lds;
+
+    for (int i = k + 1; i <= n; i++) {
+        lds.push(i);
+
+        if (lds.size() == quotient + (remainder > 0)) {
+            while (!lds.empty()) {
+                cout << lds.top() << ' ';
+
+                lds.pop();
+            }
+
+            remainder = max(remainder - 1, 0);
+        }
+    }
+
+    cout << '\n';
+
+    return 0;
+}
